d2683c4315d398983c41bd7d999e3bff2ed5b8e9
Nick Landry <nick.landry@microsoft.com>
2018-02-08 17:48:28 -0500

    Added input field to select the file to upload + test video.

--- Assets/AzureStorageExamples/Storage/Scripts/BlockBlobMediaTest.cs
+++ Assets/AzureStorageExamples/Storage/Scripts/BlockBlobMediaTest.cs
@@ -1,29 +1,47 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 // Button event handlers for Azure Blob Storage Client demo scene
 public class BlockBlobMediaTest : MonoBehaviour
 {
+    public InputField inputFilename;
+
     public async void BlockBlobMediaUpload()
     {
+        string filename = inputFilename.text.Trim();
+        if (filename.Length == 0)
+        {
+            AzureBlobStorageClient.instance.WriteLine("Please specify the file you wish to transfer.");
+        }
         AzureBlobStorageClient.instance.ClearOutput();
         AzureBlobStorageClient.instance.WriteLine("-- Uploading to Blob Storage --");
-        await AzureBlobStorageClient.instance.UploadStorageBlockBlobBasicOperationAsync("earth_8k.jpg");
+        await AzureBlobStorageClient.instance.UploadStorageBlockBlobBasicOperationAsync(filename);
         AzureBlobStorageClient.instance.WriteLine("-- Upload Test Complete --");
     }
 
     public async void BlockBlobMediaDownload()
     {
+        string filename = inputFilename.text.Trim();
+        if (filename.Length == 0)
+        {
+            AzureBlobStorageClient.instance.WriteLine("Please specify the file you wish to transfer.");
+        }
         AzureBlobStorageClient.instance.ClearOutput();
         AzureBlobStorageClient.instance.WriteLine("-- Downloading from Blob Storage --");
-        await AzureBlobStorageClient.instance.DownloadStorageBlockBlobBasicOperationAsync("earth_8k.jpg");
+        await AzureBlobStorageClient.instance.DownloadStorageBlockBlobBasicOperationAsync(filename);
         AzureBlobStorageClient.instance.WriteLine("-- Download Test Complete --");
     }
 
     public async void BlockBlobMediaDownloadBySegments()
     {
+        string filename = inputFilename.text.Trim();
+        if (filename.Length == 0)
+        {
+            AzureBlobStorageClient.instance.WriteLine("Please specify the file you wish to transfer.");
+        }
         AzureBlobStorageClient.instance.ClearOutput();
         AzureBlobStorageClient.instance.WriteLine("-- Downloading from Blob Storage by Segments --");
-        await AzureBlobStorageClient.instance.DownloadStorageBlockBlobSegmentedOperationAsync("earth_8k.jpg");
+        await AzureBlobStorageClient.instance.DownloadStorageBlockBlobSegmentedOperationAsync(filename);
     }
 
 }
