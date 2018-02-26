18a1e62e9c9417b5168982c586f059315123c12d
Nick Landry <nick.landry@microsoft.com>
2018-02-26 16:58:53 -0500

    Added new MR Movie Theater Scene (with MRTK) + project files cleanup

---
+++ Assets/Scripts/WorldSpaceVideo.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+public class WorldSpaceVideo : MonoBehaviour {
+
+    public Material playButtonMaterial;
+    public Material pauseButtonMaterial;
+    public Renderer playButtonRenderer;
+    public VideoClip[] videoClips;
+    public Text currentMinutes;
+    public Text currentSeconds;
+    public Text totalMinutes;
+    public Text totalSeconds;
+    public PlayHeadMover playheadMover;
+
+    private VideoPlayer videoPlayer;
+    private int videoClipIndex;
+
+    private void Awake()
+    {
+        {
+            videoPlayer = GetComponent<VideoPlayer>();
+        }
+    }
+    // Use this for initialization
+    void Start () {
+        // Need to release the video texture to clear the screen on start, 
+        // otherwise the last played frame from the last session will stick around
+        videoPlayer.targetTexture.Release();
+        // Set video clip to the first one in the array
+        videoPlayer.clip = videoClips[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (videoPlayer.isPlaying)
+        {
+            SetCurrentTimeUI();
+            playheadMover.MovePlayHead(CalculatePlayedFraction());
+        }
+	}
+
+    public void SetNextClip()
+    {
+        videoClipIndex++;
+
+        if (videoClipIndex >= videoClips.Length)
+        {
+            videoClipIndex = videoClipIndex % videoClips.Length;
+        }
+        videoPlayer.clip = videoClips[videoClipIndex];
+        SetTotalTimeUI();
+        videoPlayer.Play();
+    }
+
+    public void PlayPause()
+    {
+        if (videoPlayer.isPlaying)
+        {
+            videoPlayer.Pause();
+            playButtonRenderer.material = playButtonMaterial;
+        } else
+        {
+            SetTotalTimeUI();
+            videoPlayer.Play();
+            playButtonRenderer.material = pauseButtonMaterial;
+        }
+    }
+
+    void SetCurrentTimeUI()
+    {
+        string minutes = Mathf.Floor((int) videoPlayer.time / 60).ToString("00");
+        string seconds = ((int)videoPlayer.time % 60).ToString("00");
+
+        currentMinutes.text = minutes;
+        currentSeconds.text = seconds;
+    }
+    void SetTotalTimeUI()
+    {
+        string minutes = Mathf.Floor((int)videoPlayer.clip.length / 60).ToString("00");
+        string seconds = ((int)videoPlayer.clip.length % 60).ToString("00");
+
+        totalMinutes.text = minutes;
+        totalSeconds.text = seconds;
+    }
+
+    double CalculatePlayedFraction()
+    {
+        double fraction = (double)videoPlayer.frame / (double)videoPlayer.clip.frameCount;
+        return fraction;
+    }
+}
