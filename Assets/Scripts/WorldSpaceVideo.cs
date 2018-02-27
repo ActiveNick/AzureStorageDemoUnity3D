35aa164a94fe96c58033ada02e4cebb74263cb6b
Nick Landry <nick.landry@microsoft.com>
2018-02-27 15:46:59 -0500

    Video player now shows playhead progress and total time.

--- Assets/Scripts/WorldSpaceVideo.cs
+++ Assets/Scripts/WorldSpaceVideo.cs
@@ -4,50 +4,65 @@ using UnityEngine;
 using UnityEngine.Video;
 using UnityEngine.UI;
 using System.IO;
+using System.Threading.Tasks;
 
 public class WorldSpaceVideo : MonoBehaviour {
 
+    // Used to control the appearance of the Play button, with pause toggle
     public Material playButtonMaterial;
     public Material pauseButtonMaterial;
     public Renderer playButtonRenderer;
-    public string[] videoClips;
+    // Since we're loading videos by url, we're not storing actual VideoClip objects, just file names
+    public string[] videoClips;  
+    // Used to track video length and current position during playback
     public Text currentMinutes;
     public Text currentSeconds;
     public Text totalMinutes;
     public Text totalSeconds;
     public PlayHeadMover playheadMover;
-
+    // Used to control playback, we get these from the current gameobject
     private VideoPlayer videoPlayer;
     private AudioSource audioSource;
+    // The index of the current video being played
     private int videoClipIndex;
+    private bool isTotalTimeSet = false;
 
     private void Awake()
     {
-        {
-            videoPlayer = GetComponent<VideoPlayer>();
-            audioSource = gameObject.AddComponent<AudioSource>();
-
-        }
+        // Obtain video playback components from current gameobject
+        videoPlayer = GetComponent<VideoPlayer>();
+        audioSource = gameObject.AddComponent<AudioSource>();
     }
     // Use this for initialization
-    void Start () {
+    async void Start () {
         // Need to release the video texture to clear the screen on start, 
         // otherwise the last played frame from the last session will stick around
         videoPlayer.targetTexture.Release();
-        // Set video clip to the first one in the array
-        PrepareVideoFromFile(videoClips[0]);
+        // Set current video clip to the first one in the array, downloads it if needed
+        await PrepareVideoFromFile(videoClips[0]);
     }
 	
 	// Update is called once per frame
 	void Update () {
+        // If a video is currently being played... 
 		if (videoPlayer.isPlaying)
         {
+            // This will only update the total time count for the video once. We need to call in update
+            // loop since the total frame count isn;t set until the video actually plays.
+            SetTotalTimeUI(false);
+            // Update the current time index in the canvas + move the playhead based on progress
             SetCurrentTimeUI();
             playheadMover.MovePlayHead(CalculatePlayedFraction());
         }
 	}
 
-    public async void PrepareVideoFromFile(string videofile)
+    /// <summary>
+    /// Prepares a video file for playback by loading it in the video player + initializing audio.
+    /// If the file, does not exist, it is downloaded from Azure blob storage first, using default override settings.
+    /// All files are downloaded to the TempCache app folder, and played from there.
+    /// </summary>
+    /// <param name="videofile">The file name (no path) of the video to be played.</param>
+    public async Task PrepareVideoFromFile(string videofile)
     {
         string localvideofile = await AzureBlobStorageClient.instance.DownloadStorageBlockBlobSegmentedOperationAsync(videofile);
 
@@ -63,6 +78,9 @@ public class WorldSpaceVideo : MonoBehaviour {
             videoPlayer.EnableAudioTrack(0, true);
             videoPlayer.SetTargetAudioSource(0, audioSource);
 
+            SetTotalTimeUI(true);
+
+            // Preparation is not needed for videos loaded from url
             //videoPlayer.Prepare();
             ////Wait until video is prepared
             //while (!videoPlayer.isPrepared)
@@ -74,7 +92,10 @@ public class WorldSpaceVideo : MonoBehaviour {
         }
     }
 
-    public void SetNextClip()
+    /// <summary>
+    /// Increment/reset the video clip index and prepare it for playback.
+    /// </summary>
+    public async void SetNextClip()
     {
         videoClipIndex++;
 
@@ -82,27 +103,31 @@ public class WorldSpaceVideo : MonoBehaviour {
         {
             videoClipIndex = videoClipIndex % videoClips.Length;
         }
-        PrepareVideoFromFile(videoClips[videoClipIndex]);
-        SetTotalTimeUI();
+        await PrepareVideoFromFile(videoClips[videoClipIndex]);
         
         //videoPlayer.Play();
         //playButtonRenderer.material = pauseButtonMaterial;
     }
 
+    /// <summary>
+    /// Toggle between Play & Pause on current video queued based on playback state.
+    /// </summary>
     public void PlayPause()
     {
         if (videoPlayer.isPlaying)
         {
             videoPlayer.Pause();
-            playButtonRenderer.material = playButtonMaterial;
+            playButtonRenderer.material = playButtonMaterial; // Change button appearance to Play
         } else
         {
-            SetTotalTimeUI();
             videoPlayer.Play();
-            playButtonRenderer.material = pauseButtonMaterial;
+            playButtonRenderer.material = pauseButtonMaterial; // Change button appearance to Pause
         }
     }
 
+    /// <summary>
+    /// Updates the player canvas to reflect the current time index being played in the video.
+    /// </summary>
     void SetCurrentTimeUI()
     {
         string minutes = Mathf.Floor((int) videoPlayer.time / 60).ToString("00");
@@ -111,19 +136,33 @@ public class WorldSpaceVideo : MonoBehaviour {
         currentMinutes.text = minutes;
         currentSeconds.text = seconds;
     }
-    void SetTotalTimeUI()
+    void SetTotalTimeUI(bool reset)
     {
-        //string minutes = Mathf.Floor((int)videoPlayer.clip.length / 60).ToString("00");
-        //string seconds = ((int)videoPlayer.clip.length % 60).ToString("00");
+        string minutes = "00";
+        string seconds = "00";
 
-        //totalMinutes.text = minutes;
-        //totalSeconds.text = seconds;
+        if (reset)
+        {
+            isTotalTimeSet = false;
+        }
+        else if (!isTotalTimeSet && (videoPlayer.frameCount > 0))
+        {
+            float videolength = videoPlayer.frameCount / videoPlayer.frameRate;
+            minutes = Mathf.Floor((int)videolength / 60).ToString("00");
+            seconds = ((int)videolength % 60).ToString("00");
+            isTotalTimeSet = true;
+        } else
+        {
+            return;
+        }
+
+        totalMinutes.text = minutes;
+        totalSeconds.text = seconds;
     }
 
     double CalculatePlayedFraction()
     {
-        //double fraction = (double)videoPlayer.frame / (double)videoPlayer.clip.frameCount;
-        //return fraction;
-        return 0;
+        double fraction = (double)videoPlayer.frame / (double)videoPlayer.frameCount;
+        return fraction;
     }
 }
--- README.md
+++ README.md
@@ -1,17 +1,23 @@
 # AzureStorageDemoUnity3D
-Sample Unity project used to demonstrate the use of Azure Storage services in games and Mixed Reality projects. This app shows how you can upload/download blobs from Azure Storage using the basic Azure Storage SDK (I’m using the Win8 DLL for UWP), and you'll also have the ability to download blobs by segments of 1MB (configurable) which opens the door for tracking progress during the download.
+Sample Unity project used to demonstrate the use of Azure Storage services in games and Mixed Reality projects. This app shows how you can upload/download blobs from Azure Storage using the basic Azure Storage SDK (I’m using the Win8 DLL for UWP), and you also get the ability to download blobs by segments of 1MB (configurable) which opens the door for tracking progress during the download.
 
 * **Unity version: 2017.2.1p4** [download here](https://beta.unity3d.com/download/1992a1ed2d78/UnityDownloadAssistant-2017.2.1p4.exe?_ga=2.144542141.908717835.1519654169-26152681.1510325491)
 * **Mixed Reality Toolkit version: 2017.2.1.1** [download here](https://github.com/Microsoft/MixedRealityToolkit-Unity/releases/tag/2017.2.1.1)
 
 # Demo Scenes
-* AzureBlobStorageTest: Simple scene to demonstrate how to upload & download files to/from Azure Blob Storage, showcasing the Azure Transfer ProgressBar and the debug window. The spheres demonstrate the use of the BlobStorageTextureDownloader script.
-* AzureBlobStorageVideoTest: Simple movie theater scene used to play video files downloaded from Azure Blob Storage. This scene reuses components and scripts from the [Unity Graphics Live Training: Playing Video in Unity](https://unity3d.com/learn/tutorials/topics/graphics/introduction-and-session-goals?playlist=17102).
+
+* **AzureBlobStorageTest**: Simple scene to demonstrate how to upload & download files to/from Azure Blob Storage, showcasing the Azure Transfer ProgressBar and the debug window. The spheres demonstrate the use of the BlobStorageTextureDownloader script.
+* **AzureBlobStorageVideoTest**: Simple movie theater scene used to play video files downloaded from Azure Blob Storage. Press the Play button to start the first video clip, and use the Skip button to queue-up the next video clip. Sample videos are provided in the StreamingAssets folder, and you can use the *AzureBlobStorageTest* scene to upload your videos to blob storage before using them in this scene.
+
+![Movie Theater Demo Scene](Screenshots/MovieTheaterSceneView01.PNG)
 
 ## Implementation Notes
-* The plugin DLL files are included in the source since they are specific versions selected for this project based on compatibility. The [Azure Storage DLL](https://www.nuget.org/packages/WindowsAzure.Storage/) is the Win8 version for the UWP build, and the .NET 4.5 version for the Unity editor.
+
 * Note that this requires the .NET Scripting backend and support for .NET 4.6 Experimental in Unity. I have not tested this demo with IL2CPP yet.
-* When running in the editor you CANNOT use https to connect to Blob Storage due to a certificates limitation in Unity, but https works fine in the UWP build.
+* Use the *AzureBlobStorageManager* prefab in your scene since it contains the main *AzureBlobStorageClient* script & proxy class you need to quickly integrate Azure Blob Storage in your projects.
+* When running in the editor you CANNOT use https to connect to Blob Storage due to a certificates limitation in Unity, but https works fine in the UWP build. The *AzureBlobStorageClient* script includes a checkbox to enable this http/https toggle automatically for you.
+* The plugin DLL files are included in the source since they are specific versions selected for this project based on compatibility. The [Azure Storage DLL](https://www.nuget.org/packages/WindowsAzure.Storage/) is the Win8 version for the UWP build, and the .NET 4.5 version for the Unity editor.
+* The Movie Theater scene used some components and scripts from the [Unity Graphics Live Training: Playing Video in Unity](https://unity3d.com/learn/tutorials/topics/graphics/introduction-and-session-goals?playlist=17102) as a starting point.
 * There is an extra button that showcases a blob download example script using the [Azure Storage Data Movement library](https://www.nuget.org/packages/Microsoft.Azure.Storage.DataMovement) and while it currently runs fine in the Unity editor, note that the UWP build export doesn't work. This is because Unity cannot handle a 16299 UWP DLL, so I decided to shelve DMLib for now until Unity 2018.1 adds support for .NET Standard 2.0.
 * If you want to dig into the techniques used for Azure Storage in this Unity demo, check out my [UWP XAML Test Client for Azure Storage here](https://github.com/ActiveNick/AzStorageDataMovementTest).
 
